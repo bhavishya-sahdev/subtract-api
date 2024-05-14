91f8ac2d70913d240a3d2c555c3a0632ac962f29
Bhavishya <bhavishya1102@gmail.com>
2024-05-14 20:15:42 +0530

    disable cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono"
+import { cors } from "hono/cors"
 import { auth } from "./routes/auth"
 import { data } from "./routes/data"
 
 const app = new Hono()
+app.use(cors({ origin: "*" }))
 
 app.get("/helloworld", (c) => {
     return c.json({ data: "res" })
