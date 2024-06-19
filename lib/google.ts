70993b69a05fd8b9ba17a393fde59709094813f5
Bhavishya <bhavishya1102@gmail.com>
2024-06-19 17:56:25 +0530

    feat: Add Google authentication functionality to lib/google.ts

---
+++ lib/google.ts
@@ -0,0 +1,7 @@
+import { google } from "googleapis"
+
+export const authClient = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI
+)
