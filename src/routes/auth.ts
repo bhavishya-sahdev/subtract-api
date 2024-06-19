6abcfa396a3102fd44d2ec2ca3bdaedd7b1abe0e
Bhavishya <bhavishya1102@gmail.com>
2024-06-19 17:57:12 +0530

    feat: Add Google authentication functionality to auth routes
    This commit adds Google authentication functionality to the auth routes. It includes the addition of a new route "/google" for handling Google authentication requests. The code makes use of the "lib/google" module and the "axios" package to authenticate with Google and retrieve user information. The user's Google credentials are stored in the database and a JWT token is generated for authentication. This feature allows users to sign in using their Google accounts, enhancing the authentication options available to users.

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono"
 import { hash, compare } from "bcrypt"
 import { generateToken } from "lib/jwt"
-import { findUserByEmail, insertUser } from "db/schema/user"
+import { findUserByEmail, insertUser, updateUser } from "db/schema/user"
 import { z } from "zod"
 import { setCookie } from "hono/cookie"
 import { addHours } from "date-fns"
+import { authClient } from "lib/google"
+import axios from "axios"
 
 export const auth = new Hono()
 
@@ -82,6 +84,13 @@ auth.post("/signin", async (c) => {
             )
         }
 
+        if (foundUser[0].isGoogleUser) {
+            return c.json(
+                { error: "Please sign in using Google", data: null },
+                401
+            )
+        }
+
         const isValidPassword = await compare(
             password,
             foundUser[0].hashedPassword
@@ -95,6 +104,84 @@ auth.post("/signin", async (c) => {
         }
 
         const token = generateToken({ userId: foundUser[0].uuid })
+        setCookie(c, "token", token, {
+            secure: process.env.NODE_ENV === "development" ? false : true,
+            httpOnly: true,
+            domain: "localhost",
+            sameSite: "Lax",
+            expires: addHours(new Date(), 1),
+        })
+        return c.json({ data: { token }, error: null })
+    } catch (err: any) {
+        return c.json({ error: err.message, data: null }, 400)
+    }
+})
+
+auth.post("/google", async (c) => {
+    const { code } = await c.req.json()
+
+    try {
+        const { tokens } = await authClient.getToken(code)
+
+        if (
+            !tokens.access_token ||
+            !tokens.refresh_token ||
+            !tokens.expiry_date
+        )
+            return c.json({ error: "Invalid Google response", data: null }, 500)
+
+        authClient.setCredentials(tokens)
+
+        const userInfoResponse = await axios.get(
+            "https://www.googleapis.com/oauth2/v2/userinfo",
+            {
+                headers: {
+                    Authorization: `Bearer ${tokens.access_token}`,
+                },
+            }
+        )
+
+        const userInfo = userInfoResponse.data
+
+        const foundUser = await findUserByEmail(userInfo.email)
+
+        if (foundUser.length > 0) {
+            if (!foundUser[0].isGoogleUser) {
+                updateUser(foundUser[0].uuid, {
+                    isGoogleUser: true,
+                    googleId: userInfo.id,
+                    googleAccessToken: tokens.access_token,
+                    googleRefreshToken: tokens.refresh_token,
+                    googleTokenExpiresAt: tokens.expiry_date.toString(),
+                })
+            }
+
+            // generate JWT token
+            const token = generateToken({ userId: foundUser[0].uuid })
+            setCookie(c, "token", token, {
+                secure: process.env.NODE_ENV === "development" ? false : true,
+                httpOnly: true,
+                domain: "localhost",
+                sameSite: "Lax",
+                expires: addHours(new Date(), 1),
+            })
+            return c.json({ data: { token }, error: null })
+        }
+
+        // insert new user
+        const newUser = await insertUser({
+            name: userInfo.name,
+            email: userInfo.email,
+            hashedPassword: "",
+            isGoogleUser: true,
+            googleId: userInfo.id,
+            googleAccessToken: tokens.access_token,
+            googleRefreshToken: tokens.refresh_token,
+            googleTokenExpiresAt: tokens.expiry_date.toString(),
+        })
+
+        // generate JWT token
+        const token = generateToken({ userId: newUser[0].insertedUserId })
         setCookie(c, "token", token, {
             secure: process.env.NODE_ENV === "development" ? false : true,
             httpOnly: true,
