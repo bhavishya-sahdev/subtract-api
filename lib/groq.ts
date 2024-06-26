cde9b169bfb6d54212df69520a638b1cbe68a783
Bhavishya <bhavishya1102@gmail.com>
2024-06-26 17:43:56 +0530

    refactor: Update groq.ts to improve email classification and extraction logic

--- lib/groq.ts
+++ lib/groq.ts
@@ -5,16 +5,18 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY })
 const SYSTEM_PROMPT = `
 You classify emails and extract required information in JSON. 
 If the email is related to a subscription service payment or notification then the schema should look like: 
-{isSubscription: true, 
-data: {
-renewal_amount: number,
-renewal_amount_currency_code: string,
-renewal_period_enum: one of "monthly" | "annually" | "weekly" | "custom",
-renewal_period_days: 1 if renewal_period_enum is not custom else should be a time period in ISO date format,
-service_name: string,
-next_renewal_date: date string,
-current_payment_date: date string of when this payment took place
-}}
+{
+    isSubscription: true, 
+    data: {
+        renewal_amount: number,
+        renewal_amount_currency_code: string,
+        renewal_period_enum: one of "monthly" | "annually" | "weekly" | "custom",
+        renewal_period_days: 1 if renewal_period_enum is not custom else should be a time period in just days,
+        service_name: string,
+        next_renewal_date: date string in the format "YYYY-MM-DD",
+        current_payment_date: date string of when this payment took place
+    }
+}
 and if it's not a subscription then just return {isSubscription: false}
 `
 
@@ -37,7 +39,7 @@ export const extractEmailData = async (
                     };`,
                 },
             ],
-            model: "llama3-8b-8192",
+            model: "llama3-70b-8192",
             temperature: 0,
             stream: false,
             response_format: { type: "json_object" },
